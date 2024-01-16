86d087e3db23226e1e848f8c7934c3f515943dd4
ttungbmt <ttungbmt@gmail.com>
2024-01-16 23:51:06 +0700

    Update changes

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+# conn = st.connection("api_reverse_crawler")
+# df = conn.query("select * from alembic_version")
+# st.dataframe(df)
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
